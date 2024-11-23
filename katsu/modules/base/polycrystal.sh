9987e72dfe028c5d75e3f884d7302313cf616c3e
Lleyton Gray <lleyton@fyralabs.com>
2024-11-22 17:01:43 -0800

    try workaround

--- katsu/modules/base/polycrystal.sh
+++ katsu/modules/base/polycrystal.sh
@@ -1,4 +1,23 @@
 #!/bin/bash -x
+set -euo pipefail
+
+cleanup() {
+    for mnt in sys proc; do
+        umount /mnt/mock-mount/$mnt
+    done
+    umount /mnt/mock-mount
+    umount /mnt/mock-mount
+}
+
 if [ -x "$(command -v polycrystal)" ]; then
-    polycrystal
+    trap cleanup EXIT
+    mkdir -p /mnt/mock-mount
+    mount --bind / /mnt/mock-mount
+    mount --make-private /mnt/mock-mount
+    mount --bind /mnt/mock-mount /mnt/mock-mount
+    for mnt in proc sys; do
+        mount --bind /$mnt /mnt/mock-mount/$mnt
+    done
+
+    chroot /mnt/mock-mount bash -c polycrystal
 fi
